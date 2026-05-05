850a2c5ae1e8b5913def5576e11143cf79103a9e
GabKenKL <gabkenkudodelima@gmail.com>
2026-05-05 10:14:42 -0300

    Primeira versão dos códigos

---
+++ ex1. Área do Quadrilátero regular.py
@@ -0,0 +1,8 @@
+print("Área do quadrilátero regular - (QUADRADO)")
+print("Digite o tamanho do lado:")
+
+lado = int(input())
+
+area = lado * lado
+
+print("A área do quadrado de lado:", lado , "vale:" , area)
